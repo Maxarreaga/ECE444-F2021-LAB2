5f1399f72812fc2fa069cd2f08ad91c0016a8c89
Maxarreaga <34686490+Maxarreaga@users.noreply.github.com>
2021-09-21 23:34:54 -0400

    activity 3 working and screenshoted, merged the hellos into one file

--- hello.py
+++ hello.py
@@ -1,9 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from datetime import datetime
+from flask_moment import Moment
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html', name='Max', current_time=datetime.utcnow())
 
-if __name__== '__main__':
-        app.run(debug=True)
+
+@app.route('/user/<name>')
+def user(name):
+    # the response document we give the user
+    return render_template('user.html', name=name, current_time=datetime.utcnow())
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
